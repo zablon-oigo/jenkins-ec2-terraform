17597dc291bdb70ed39a980f684accb5773c90ba
zablon <zablonoigo51@gmail.com>
2024-06-22 13:18:34 +0300

    Adding custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from .managers import CustomUserManager
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email=models.EmailField(unique=True)
+    is_staff=models.BooleanField(default=False)
+    is_active=models.BooleanField(default=True)
+    date_joined=models.DateField(default=timezone.now)
 
-# Create your models here.
+    USERNAME_FIELD="email"
+    REQUIRED_FIELDS=[]
+    objects=CustomUserManager()
+
+    def __str__(self):
+        return self.email
