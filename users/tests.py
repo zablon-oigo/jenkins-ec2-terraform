5fc6554d4914fbbd439e8d0a7e126b1e44d74cc9
zablon <zablonoigo51@gmail.com>
2024-06-23 14:04:26 +0300

    test user manager

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
-# Create your tests here.
+from django.contrib.auth import get_user_model
+User=get_user_model()
+class UsersManagersTest(TestCase):
+    def test_create_user(self):
+        user=User.objects.create_user(email="user@mail.com", password="secret")
+        self.assertEqual(user.email, "user@mail.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
