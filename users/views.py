932ee03c64c306509e70cea06578daeee09750dd
zablon <zablonoigo51@gmail.com>
2024-06-27 11:41:54 +0300

    Adding activate email

--- users/views.py
+++ users/views.py
@@ -2,6 +2,27 @@ from django.shortcuts import render,redirect
 from .models import CustomUser
 from django.contrib import messages
 from .forms import SignUpForm
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.views import PasswordResetView,PasswordResetConfirmView
+from django.core.mail import send_mail
+from .tokens import account_activation_token
+
+def activeEmail(request, user, to_email):
+    context={
+        'user':user,
+        'domain':get_current_site(request).domain,
+        'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+        'token':account_activation_token.make_token(user),
+        'protocol':'https' if request.is_secure() else 'http'
+    }
+    email_content=render_to_string('users/acct_active_email.html', + context)
+
 def signup(request):
     if request.method == 'POST':
         form=SignUpForm(request.POST)
