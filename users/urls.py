e94c808a83a77a9688390a517880313fa228cbe3
zablon <zablonoigo51@gmail.com>
2024-06-29 08:55:46 +0300

    adding password  reset path

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,7 @@ urlpatterns=[
     path('login/',login,name='login'),
     path('logout/',custom_logout,name='logout'),
     path('activate/<uidb64>/<token>',activate,name='activate'),
-    path('password-reset/',CustomPasswordResetView,name='password_reset'),
-    path('reset/<uidb64>/<token>/',CustomPasswordResetConfirmView,name='password_reset_confirm'),
+    path('password_reset/',CustomPasswordResetView.as_view(),name='password_reset'),
+    path('reset/<uidb64>/<token>/',CustomPasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
 ]
