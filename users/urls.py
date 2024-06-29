ddac318f37fcd33dfcc907f07a2ee253afb3faa6
zablon <zablonoigo51@gmail.com>
2024-06-29 08:47:52 +0300

    Adding password reset path

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import *
-
+from django.contrib.auth import views as auth_views
 urlpatterns=[
     path('', index, name='index'),
     path('register/',signup, name='signup'),
     path('login/',login,name='login'),
     path('logout/',custom_logout,name='logout'),
     path('activate/<uidb64>/<token>',activate,name='activate'),
+    path('password-reset/',CustomPasswordResetView,name='password_reset'),
+    path('reset/<uidb64>/<token>/',CustomPasswordResetConfirmView,name='password_reset_confirm'),
 ]
