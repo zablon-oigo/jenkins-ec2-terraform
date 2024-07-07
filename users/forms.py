8982f0dab17c45b162c33b4fb5e4816a7f1bc34f
zablon <zablonoigo51@gmail.com>
2024-07-07 12:53:21 +0300

    Changed background color

--- users/forms.py
+++ users/forms.py
@@ -32,9 +32,9 @@ class SignUpForm(UserCreationForm):
 class LoginForm(forms.Form):
     email=forms.CharField(widget=forms.EmailInput(attrs={
             'placeholder':'Enter your email',
-            'class':'px-6 py-4 w-full rounded-xl mb-2'
+            'class':'px-6 py-4 w-full bg-gray-50 rounded-xl mb-2'
         }))
     password=forms.CharField(widget=forms.PasswordInput(attrs={
             'placeholder':'Enter your password',
-            'class':'w-full px-6 py-4 rounded-xl mb-2',
+            'class':'w-full bg-gray-50 px-6 py-4 rounded-xl mb-2',
         }))
