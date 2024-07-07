99c5d461f3267685374411a10fb8ab97fb3c553e
zablon <zablonoigo51@gmail.com>
2024-07-07 13:22:35 +0300

    Changed form input BackgroundColor

--- users/forms.py
+++ users/forms.py
@@ -17,15 +17,15 @@ class SignUpForm(UserCreationForm):
         fields=('email','password1','password2')
     email=forms.CharField(widget=forms.EmailInput(attrs={
             'placeholder':'Enter your email',
-            'class':'px-6 py-4 w-full rounded-xl mb-2'
+            'class':'px-6 py-4 bg-gray-50 w-full rounded-xl mb-2'
         }))
     password1=forms.CharField(widget=forms.PasswordInput(attrs={
             'placeholder':'Enter your password',
-            'class':'w-full px-6 py-4 rounded-xl mb-2',
+            'class':'w-full px-6 py-4 bg-gray-50 rounded-xl mb-2',
         }))
     password2=forms.CharField(widget=forms.PasswordInput(attrs={
             'placeholder':'Repeat your password',
-            'class':'w-full px-6 py-4 rounded-xl mb-2'
+            'class':'w-full px-6 py-4 bg-gray-50 rounded-xl mb-2'
         }))
 
 
