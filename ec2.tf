b8b5d865f9bd024bf52ede63337d15dc1cf76ca6
zablon <zablonoigo51@gmail.com>
2024-12-09 20:13:16 +0300

    Add server

---
+++ ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "ec2" {
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = var.instance_type 
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.public-subnet.id
+  vpc_security_group_ids = [aws_security_group.security-group.id]
+  iam_instance_profile   = aws_iam_instance_profile.instance-profile.name
+}
