ff1371e422826f4c344fd10e0e2c9878758598bb
zablon <zablonoigo51@gmail.com>
2024-12-11 09:14:11 +0300

    Add a  policy statement

--- iam.tf
+++ iam.tf
@@ -1,4 +1,18 @@
 
 resource "aws_iam_role" "iam-role" {
-  name = var.iam_role_name
+  name               = var.iam_role_name
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
 }
