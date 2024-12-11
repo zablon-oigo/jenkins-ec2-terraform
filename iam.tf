47ab2e104b0a9c3bf65d78080b950a396c242bb6
zablon <zablonoigo51@gmail.com>
2024-12-11 09:15:49 +0300

    Add IAM policy

--- iam.tf
+++ iam.tf
@@ -16,3 +16,29 @@ resource "aws_iam_role" "iam-role" {
 }
 EOF
 }
+resource "aws_iam_policy" "ecr_access_policy" {
+  name        = "ECRAccessPolicy"
+  description = "Policy to allow EC2 to access ECR"
+    policy      = <<EOF
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Effect": "Allow",
+        "Action": [
+            "ecr:GetAuthorizationToken",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:GetRepositoryPolicy",
+            "ecr:ListImages",
+            "ecr:BatchGetImage",
+            "ecr:InitiateLayerUpload",
+            "ecr:UploadLayerPart",
+            "ecr:CompleteLayerUpload",
+            "ecr:PutImage"
+        ],
+        "Resource": "*"
+        }
+    ]
+    }
+    EOF
+}
