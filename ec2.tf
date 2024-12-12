42f8339ef55114cbce78a3fe65fb3c8a4cf8556b
zablon <zablonoigo51@gmail.com>
2024-12-12 21:36:29 +0300

    Improve formatting

--- ec2.tf
+++ ec2.tf
@@ -5,11 +5,14 @@ resource "aws_instance" "ec2" {
   subnet_id              = aws_subnet.public-subnet.id
   vpc_security_group_ids = [aws_security_group.security-group.id]
   iam_instance_profile   = aws_iam_instance_profile.instance-profile.name
-    root_block_device {
+
+  root_block_device {
     volume_size = 30
   }
+
   user_data = templatefile("./installation.sh", {})
-tags = {
-    Name        = var.instance_name
+
+  tags = {
+    Name = var.instance_name
   }
 }
