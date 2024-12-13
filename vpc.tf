b98769d645d0fcfe9d49749591e699bdab71ffab
zablon <zablonoigo51@gmail.com>
2024-12-13 14:49:57 +0300

    Format file

--- vpc.tf
+++ vpc.tf
@@ -11,16 +11,16 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 resource "aws_subnet" "public-subnet" {
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true
 }
 resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.vpc.id
   route {
-    cidr_block = "0.0.0.0/0" 
-    gateway_id = aws_internet_gateway.igw.id  
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
     Name = var.route_table_name
