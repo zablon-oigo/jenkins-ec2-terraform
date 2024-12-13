af7bc95c4c001906f0e3a94cef78432e2297dc51
zablon <zablonoigo51@gmail.com>
2024-12-13 14:46:05 +0300

    Add route for vpc

--- vpc.tf
+++ vpc.tf
@@ -16,3 +16,10 @@ resource "aws_subnet" "public-subnet" {
   availability_zone = "us-west-2a"
   map_public_ip_on_launch = true
 }
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0" 
+    gateway_id = aws_internet_gateway.igw.id  
+  }
+}
