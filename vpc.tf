5798d8d08904fc2dd618008522bdd3946609ccf4
zablon <zablonoigo51@gmail.com>
2024-12-13 14:40:09 +0300

    create a public subnet

--- vpc.tf
+++ vpc.tf
@@ -10,3 +10,9 @@ resource "aws_internet_gateway" "igw" {
     Name = var.igw_name
   }
 }
+resource "aws_subnet" "public-subnet" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = true
+}
